cc35357d8c8fcf430508153ebbee75f8a0177bc0
geonil2 <kji8922@naver.com>
2021-11-21 19:48:59 +0900

    remove api key & update state

--- src/app.jsx
+++ src/app.jsx
@@ -3,39 +3,18 @@ import styles from './app.module.css';
 import VideoList from './Components/VideoList/VideoList';
 import SearchHeader from './Components/SearchHeader/SearchHeader'
 
-function App() {
+function App({ youtube }) {
   const [videos, setVideos] = useState([]);
   const search = query => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow',
-    };
-
-    fetch(
-      `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyB21z53plGDO_6xN1dwp_qjp6RIg7Obt9Q`,
-      requestOptions
-    )
-      .then(response => response.json())
-      .then(result => 
-        result.items.map(item => ({...item, id: item.id.videoId}))
-      )
-      .then(items => setVideos(items))
-      .catch(error => console.log('error', error));
+    youtube.search(query)
+    .then(videos => setVideos(videos));
   }
 
   useEffect(() => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow',
-    };
+    youtube.mostPopular()
+    .then(videos => setVideos(videos));
 
-    fetch(
-      'https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyB21z53plGDO_6xN1dwp_qjp6RIg7Obt9Q',
-      requestOptions
-    )
-      .then(response => response.json())
-      .then(result => setVideos(result.items))
-      .catch(error => console.log('error', error));
+    
   }, []);
   return (
     <div className={styles.app}> 
