2e31e035527dd1630cd69b92a457a78143d62dbd
geonil2 <kji8922@naver.com>
2021-11-21 19:30:17 +0900

    make search header

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,27 @@
 import React, { useEffect, useState } from 'react';
-import './app.css';
+import styles from './app.module.css';
 import VideoList from './Components/VideoList/VideoList';
+import SearchHeader from './Components/SearchHeader/SearchHeader'
 
 function App() {
   const [videos, setVideos] = useState([]);
+  const search = query => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    };
+
+    fetch(
+      `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyB21z53plGDO_6xN1dwp_qjp6RIg7Obt9Q`,
+      requestOptions
+    )
+      .then(response => response.json())
+      .then(result => 
+        result.items.map(item => ({...item, id: item.id.videoId}))
+      )
+      .then(items => setVideos(items))
+      .catch(error => console.log('error', error));
+  }
 
   useEffect(() => {
     const requestOptions = {
@@ -19,7 +37,12 @@ function App() {
       .then(result => setVideos(result.items))
       .catch(error => console.log('error', error));
   }, []);
-  return <VideoList videos={videos} />;
+  return (
+    <div className={styles.app}> 
+      <SearchHeader onSearch={search} />
+      <VideoList videos={videos} />;
+    </div>
+  )
 }
 
 export default App;
